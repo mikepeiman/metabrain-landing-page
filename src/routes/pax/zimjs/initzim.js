db71ff6faa5c6588f53c3970469bcea9f7b14ae5
Mike Peiman <mikepeiman@gmail.com>
2022-03-08 16:50:34 -0500

    deleted most unrelated routes to simplify landing page debug

--- src/routes/pax/zimjs/initzim.js
+++
@@ -1,4 +0,0 @@
-require('@danzen/createjs');
-const makeZIM = require('zimjs');
-const zim = makeZIM(); // pass in true to use zim namespace **
-const [zog,zid,zss,zgo,zum,zot,zop,zil,zet,zob,zik,zta] = zim.getGlobals();
